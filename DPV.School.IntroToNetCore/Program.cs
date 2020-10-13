ccba711971ff6420ccf5d550da9ce466d34beb51
Trufanov Roman <pingvin1308@gmail.com>
2020-10-13 10:14:14 +0300

    init

---
+++ DPV.School.IntroToNetCore/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DPV.School.IntroToNetCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
